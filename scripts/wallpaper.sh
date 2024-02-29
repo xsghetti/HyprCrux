1675a1e041460d5a67ae34da702ed568f50de777
Dante <150515748+xsghetti@users.noreply.github.com>
2024-02-29 13:39:53 -0500

    fixed script paths cuz im stupid.

---
+++ scripts/wallpaper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+DIR=~/.config/wallpapers
+PICS=($(ls ${DIR}))
+
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+if [[ $(pidof swww) ]]; then
+  pkill swww
+fi
+
+swww query || swww init
+
+#change-wallpaper using swww
+swww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 2
+
+wal -i ~/.config/rofi/.current_wallpaper
+
+~/.config/hypr/scripts/swww.sh
+
+_ps=(waybar)
+for _prs in "${_ps[@]}"; do
+    if pidof "${_prs}" >/dev/null; then
+        pkill "${_prs}"
+    fi
+done
+
+sleep 1
+# Relaunch waybar
+waybar &
+
