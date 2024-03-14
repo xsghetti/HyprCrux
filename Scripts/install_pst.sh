e6a847b043a8643389c495f02ada224932baed8a
Dante <150515748+xsghetti@users.noreply.github.com>
2024-03-14 01:31:48 -0400

    Update install_pst.sh

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -10,64 +10,6 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-
-# themepatcher
-echo -e "\033[0;32m[THEMEPATCHER]\033[0m additional themes available..."
-awk -F '"' '{print "["NR"]",$2}' themepatcher.lst
-prompt_timer 10 "Patch these additional themes? [Y/n]"
-thmopt=${promptIn,,}
-
-if [ "${thmopt}" = "y" ] ; then
-    echo -e "\033[0;32m[THEMEPATCHER]\033[0m Patching themes..."
-    while read -r themeName themeRepo themeCode
-    do
-        themeName="${themeName//\"/}"
-        themeRepo="${themeRepo//\"/}"
-        themeCode="${themeCode//\"/}"
-        ./themepatcher.sh "${themeName}" "${themeRepo}" "${themeCode}"
-    done < themepatcher.lst
-else
-    echo -e "\033[0;33m[SKIP]\033[0m additional themes not patched..."
-fi
-
-
-# sddm
-if pkg_installed sddm
-    then
-
-    echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m detected // sddm"
-    if [ ! -d /etc/sddm.conf.d ] ; then
-        sudo mkdir -p /etc/sddm.conf.d
-    fi
-
-    if [ ! -f /etc/sddm.conf.d/kde_settings.t2.bkp ] ; then
-        echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m configuring sddm..."
-        echo -e "Select sddm theme:\n1) Candy\n2) Corners"
-        read -p "Enter option number : " sddmopt
-
-        case $sddmopt in
-        1) sddmtheme="Candy";;
-        *) sddmtheme="Corners";;
-        esac
-
-        sudo tar -xzf ${CloneDir}/Source/arcs/Sddm_${sddmtheme}.tar.gz -C /usr/share/sddm/themes/
-        sudo touch /etc/sddm.conf.d/kde_settings.conf
-        sudo cp /etc/sddm.conf.d/kde_settings.conf /etc/sddm.conf.d/kde_settings.t2.bkp
-        sudo cp /usr/share/sddm/themes/${sddmtheme}/kde_settings.conf /etc/sddm.conf.d/
-    else
-        echo -e "\033[0;33m[SKIP]\033[0m sddm is already configured..."
-    fi
-
-    if [ ! -f /usr/share/sddm/faces/${USER}.face.icon ] && [ -f ${CloneDir}/Source/misc/${USER}.face.icon ] ; then
-        sudo cp ${CloneDir}/Source/misc/${USER}.face.icon /usr/share/sddm/faces/
-        echo -e "\033[0;32m[DISPLAYMANAGER]\033[0m avatar set for ${USER}..."
-    fi
-
-else
-    echo -e "\033[0;33m[WARNING]\033[0m sddm is not installed..."
-fi
-
-
 # dolphin
 if pkg_installed dolphin && pkg_installed xdg-utils
     then
